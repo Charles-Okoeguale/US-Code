17066665ac950967d85e8c81d1f6a77a101dfb23
Charles <egualecharles@gmail.com>
2022-04-08 19:10:16 +0100

    add info template

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import React, {useMemo, useState} from "react";
 import { BsFillXCircleFill } from "react-icons/bs";
 import { AiFillCheckCircle } from "react-icons/ai"
+import axios from "axios";
 
 
 function App() {
   const [ value, setValue ] = useState('')
+  const [ info, setInfo ] = useState('')
   var isValidZip = useMemo(() => value.match(/(^\d{5}$)|(^\d{5}-\d{4}$)/), [value]);
 
   const updateValue = (val) => {
-    const filteredVal = val.split().filter(text => !isNaN(text));
-    if (filteredVal.length >= 1) {
-      setValue(filteredVal.join());
-    }
+    const filteredVal = val.split().filter(val => !isNaN(val));
+    setValue(filteredVal.join());
+    if (filteredVal >= 5) {
+      axios.get(`https://www.zipwise.com/webservices/zipinfo.php?key=qnpdvgx7mss4meum&zip=${val}&format=json`)
+      .then((res) => {
+       setInfo(res.data.results.county)
+      })
+    } 
   }
 
   return (
     <div className="App">
        <div className="info">
-        <p>Type in a valid zip code</p>
+        <p>{info}</p>
       </div> 
       <div className='zipcode'>
           <input type="text"  id='zipcode'    value={value} onChange={(e) => updateValue(e.target.value)}/>
-          <label htmlFor="zipcode">Zip code...</label>
+          <label htmlFor="zipcode">Type in valid american zip code...</label>
           <span className='indicator'>
           { isValidZip ? <AiFillCheckCircle  color="green" className="ri"/> : <BsFillXCircleFill  color="orange" className="ri"/> }
           </span>  
@@ -32,6 +38,3 @@ function App() {
 }
 
 export default App;
-
-
-// @"^\d{10}$"
